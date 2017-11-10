9828d2e04839bd5da3356a7cf06d26d2419fdad0
Poulose Matthen <matthen@Pouloses-iMac.Dlink>
2017-11-10 10:18:37 +0530

    Completion

---
+++ Goyal & Co Sales App/ApartmentDetailViewPage1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ApartmentDetailViewPage1ViewController.h
+//  Goyal & Co Sales App
+//
+//  Created by Poulose Matthen on 21/09/17.
+//  Copyright © 2017 Poulose Matthen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApartmentDetailViewPage1ViewController : UIViewController
+
+@end
---
+++ Goyal & Co Sales App/ApartmentDetailViewPage3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ApartmentDetailViewPage3ViewController.h
+//  Goyal & Co Sales App
+//
+//  Created by Poulose Matthen on 21/09/17.
+//  Copyright © 2017 Poulose Matthen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApartmentDetailViewPage3ViewController : UIViewController
+
+@end
